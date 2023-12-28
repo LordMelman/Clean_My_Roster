e22e361eeb7b83c7ebb90419835b2b1e691037ab
John Marmaduke <jmarmaduke3@gatech.edu>
2023-12-27 19:18:02 -0500

    Add base stat values to minimized view

--- Roster_Rework.py
+++ Roster_Rework.py
@@ -1,4 +1,30 @@
 from bs4 import BeautifulSoup
+import re
+
+def append_values_to_heading(soup):
+    for root_selection in soup.find_all(class_='rootselection'):
+        # Find the h4 tag
+        heading = root_selection.find('h4')
+        if heading:
+            print(f"Processing: {heading.text}")
+
+            # Extract the relevant part from the h4 text
+            h4_text_part = re.split(r" \[", heading.text)[0]
+
+            # Search for the corresponding table row
+            for table in root_selection.find_all('table'):
+                trs = table.find_all('tr')
+                for tr in trs:
+                    tds = tr.find_all('td')
+                    if len(tds) > 1 and h4_text_part in tds[0].text:
+                        # Extract relevant values from tds
+                        values = [td.text.strip() for td in tds[1:7]]
+                        if len(values) >= 6:
+                            values_text = ' - M {} T {} SV {} W {} LD {} OC {}'.format(*values)
+                            heading.string = f"{heading.text} {values_text}"
+                            print(f"Updated: {heading}")
+                            break
+
 
 def add_minimize_functionality(soup):
     # Iterate through each rootselection and wrap its contents except the heading
@@ -41,6 +67,7 @@ def reformat_html(file_path):
         content = file.read()
 
     soup = BeautifulSoup(content, 'html.parser')
+    append_values_to_heading(soup)
     add_minimize_functionality(soup)
     formatted_content = soup.prettify()
 
